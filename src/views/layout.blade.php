00cdd3ca7ae517df1d34721c5328839c5b921c7c
manavo <manavo@gmail.com>
2014-12-11 12:30:24 +0000

    Include base layout file

---
+++ src/views/layout.blade.php
@@ -0,0 +1,41 @@
+<!doctype html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+
+        <title>@yield('title')</title>
+
+        <meta content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0" name="viewport" />
+        <meta name="viewport" content="width=device-width" />
+
+        {{ Manavo\LaravelToolkit\Utilities::asset('builds/app.css') }}
+        @if(!empty($css))
+            @foreach($css as $cssFile)
+            {{ Manavo\LaravelToolkit\Utilities::asset($cssFile) }}
+            @endforeach
+        @endif
+
+        <link href="http://maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" rel="stylesheet">
+
+        @yield('head')
+    </head>
+    <body>
+        @yield('nav')
+
+        <div class="container wrapper">
+            <div class="section">
+                @yield('content')
+            </div>
+        </div>
+
+        @yield('footer')
+
+        {{ Manavo\LaravelToolkit\Utilities::asset('builds/app.js') }}
+        @if(!empty($js))
+            @foreach($js as $jsFile)
+            {{ Manavo\LaravelToolkit\Utilities::asset($jsFile) }}
+            @endforeach
+        @endif
+    </body>
+</html>
